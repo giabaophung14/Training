811dc84fe75f2bf4769b84462d74e780b20c5d7b
giabaophung14 <48116817+giabaophung14@users.noreply.github.com>
2022-07-13 17:19:42 +0700

    hoàn thành bai tap 1

--- Exercise/loop/solve/bai-1/main.js
+++ Exercise/loop/solve/bai-1/main.js
@@ -1,11 +1,37 @@
-var date = new Date();
+var  test = "hello";
+function tong(x,y){
+    
 
-var year = date.getFullYear();
-var month = date.getMonth() +1;
-var day = date.getDay();
-var hour = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
 
-console.log(`${day}/${month}/${year}`);
-console.log(`${hour}:${minutes}:${seconds}`);
+
+
+
+    if (isNaN(x) == true || isNaN(y) == true) {
+      return false;
+    }
+
+    // if(isNaN != true || isNaN != null){
+    //     return false;
+    // }
+
+    if ( x > y) {
+      return false;
+    }
+    var sum = 0;
+    for(var i = x; i <= y; i++) {
+      sum = sum + i;
+    }
+
+    if (x == 0){
+        return false;
+    } else if (sum == 1) {
+        return true;
+    }
+
+    return sum;
+
+  }
+  tong(1,100);
+  
+  
+  document.getElementById("total").innerHTML = tong(1, 100);
