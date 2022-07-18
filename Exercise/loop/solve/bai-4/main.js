cb7c3901f622f0418e38de9bbbb71ee7cfb69813
giabaophung14 <48116817+giabaophung14@users.noreply.github.com>
2022-07-18 09:24:03 +0700

    Hoan thanh bai tap so 4

--- Exercise/loop/solve/bai-4/main.js
+++ Exercise/loop/solve/bai-4/main.js
@@ -1,7 +1,12 @@
-function myArray(value) {
-   return value < 0
- }
- 
- var filtered = [5, -2 , 87 , 8 , -0.1 , 0.1 , 9 , 25, -2.5, 10 , 8 , -2 , 3 , - 3 , 2.9 , 8.9 , 108 , 40.5 , -90].filter(myArray);
+var array = [5, -2 , 87 , 8 , -0.1 , 0.1 , 9 , 25, -2.5, 10 , 8 , -2 , 3 , - 3 , 2.9 , 8.9 , 108 , 40.5 , -90];
 
-console.log(filtered)
+function myArray(num) {
+  for (var i = 1; num < i; i--) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return num < 0;
+}
+
+console.log(array.filter(myArray));
