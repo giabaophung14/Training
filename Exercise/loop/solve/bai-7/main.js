0283f52e19ad904d45b48aaac06c7343c8a23e44
giabaophung14 <48116817+giabaophung14@users.noreply.github.com>
2022-07-18 15:53:16 +0700

    Hoan thanh bai tap 7

---
+++ Exercise/loop/solve/bai-7/main.js
@@ -0,0 +1,11 @@
+var array = [5, -2 , 87 , 8 , -0.1 , 0.1 , 9 , 25, -2.5, 10 , 8 , -2 , 3 , - 3 , 2.9 , 8.9 , 108 , 40.5 , -90];
+ 
+var arrayLength = array.length
+var valid_array = []
+for (var i = 0; i < arrayLength; i ++){
+  if (Math.abs(array[i] >Math.abs(array[i+1]))) {
+    valid_array.push(array[i]);
+  } 
+}
+
+console.log(valid_array)
