dbb38646f3aea7238bb9b9d8fddafa807161a585
giabaophung14 <48116817+giabaophung14@users.noreply.github.com>
2022-07-20 11:06:24 +0700

    Fix bai tap 1 function

--- Exercise/function/solve/bai-1/main.js
+++ Exercise/function/solve/bai-1/main.js
@@ -2,6 +2,8 @@ var array= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 function multipleNumber(x){
 
+    var valid_array = [];
+
     var arrayLength = array.length;
 
     if(x % 1 !=0){
@@ -11,9 +13,12 @@ function multipleNumber(x){
     for(var i = 0; i <= arrayLength; i++){
         if(array[i] % x == 0){
             console.log(array[i])
+            valid_array.push(array[i])
         }
-
     }
 
+    return valid_array;
+
 }
-multipleNumber(3);
+
+document.getElementById("multipleNumber").innerHTML = multipleNumber(2);
