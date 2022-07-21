5b8d4c720e14f39d7be24c8eaf0f1754c833020a
giabaophung14 <48116817+giabaophung14@users.noreply.github.com>
2022-07-21 16:32:38 +0700

    Hoan thanh bai tap 5 Function

--- Exercise/function/solve/bai-5/main.js
+++ Exercise/function/solve/bai-5/main.js
@@ -1,21 +1,36 @@
 function checkPrime(n) {
-    if( Number(n) === n && n % 1 !== 0){
+    if (Number(n) === n && n % 1 !== 0) {
         return false;
     }
 
-    if(n < 2){
+    if (n < 2) {
         return false;
     }
 
-    for(i = 2; i < n/2; i ++){
+    for (i = 2; i < n; i++) {
 
-        if(n % i == 0){
+        if (n % i == 0) {
             return false;
         }
-        
+
     }
 
     return true;
 }
 
-document.getElementById("checkPrime").innerHTML = checkPrime(7)
+function primeArray(x) {
+
+    var array = [];
+
+    for (var i = 0; i < x.length; i++) {
+
+        if (checkPrime(x[i])) {
+
+            array.push(x[i])
+
+        }
+    }
+    return array;
+}
+
+document.getElementById("checkPrime").innerHTML = primeArray([17,18,19,20,21,22,23,24,25,26,27,28,29,30]);
