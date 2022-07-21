1d28fa8704a91609ff8f4f07798ee7d53b87299c
cuong_nt <cuong-nt@blockhive.vn>
2022-07-21 09:36:36 +0700

    Review 14 loop , 1 function

--- Exercise/function/solve/bai-1/main.js
+++ Exercise/function/solve/bai-1/main.js
@@ -1,5 +1,9 @@
 var array= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-
+// hinh nhu sai de 
+// vd Tìm bội số của 2
+//  Áp dụng kiến thức vừa học, ta lấy 2 nhân lần lượt với n = 0, 1, 2, 3, 4, 5,...
+//  Vậy bội số của 2 là 0, 2, 4, 6, 8, 10, ...
+// // Nhu v đc vô hạn , nên lấy từ 1 -> 15 thôi
 function multipleNumber(x){
 
     var valid_array = [];
--- Exercise/loop/solve/bai-14/main.js
+++ Exercise/loop/solve/bai-14/main.js
@@ -1,6 +1,8 @@
 var array = [5, -2, 87, 8, -0.1, 0.1, 9, 25, -2.5, 10, 8, -2, 3, - 3, 2.9, 8.9, 108, 40.5, -90];
 
 var arrayLength = array.length;
+// sao log ra 19 lần -2 ???
+// Kết quả phải là 11 (vị trí 11 có giá trị -2  bằng với số âm đầu tiên là -2 ở vị trí 1 )
 
 function checkNumber() {
     for (var i = 0; i < arrayLength; i++) {
