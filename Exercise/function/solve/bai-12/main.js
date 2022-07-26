bea70aae1a5760ca058d9684e292a044f2b154fd
cuong_nt <cuong-nt@blockhive.vn>
2022-07-26 17:21:37 +0700

    Review Bài 12

--- Exercise/function/solve/bai-12/main.js
+++ Exercise/function/solve/bai-12/main.js
@@ -2,12 +2,12 @@ function number(x){
 
     var y = 0;
 
+    // Tại sao không phải là 
+    // if(x % 1 == 0) return x+1 
+    // Loop de lam gì
     for(var i = 0; i <= x; i++){
-
         if(i % 1 == 0 && i == x){
-
             y = i + 1;
-
         }
 
     }
